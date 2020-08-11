69b06a4ef68b35ed27d74b0235cd8bf5ab40bdd3
Wilson Júnior <wilson.junior@armateus.com.br>
2020-08-11 13:55:47 -0300

    Modulo Starter

---
+++ Starter/Promises/main.js
@@ -0,0 +1,20 @@
+var minhaPromise = function() {
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/j1ni0r');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 ) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject('Erro na requisição');
+                }
+            }
+        }
+    });
+}
+
+var resultado = minhaPromise();
+console.log(resultado);
